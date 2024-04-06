d24e6fdd66fd3dc41d5e1a5e3421d95ab2856d73
nitish singh <itsnitish881@gmail.com>
2024-04-06 11:46:07 +0530

    added contact and feedback pages

--- frontend/src/app/(main)/contact/page.jsx
+++ frontend/src/app/(main)/contact/page.jsx
@@ -1,9 +1,235 @@
-import React from 'react'
+
+import React from 'react';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <>
+        <section className="bg-blue-50 dark:bg-slate-800" id="contact">
+          <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8 lg:py-20">
+            <div className="mb-4">
+              <div className="mb-6 max-w-3xl text-center sm:text-center md:mx-auto md:mb-12">
+
+                <h2 className="font-heading mb-4 font-bold tracking-tight text-gray-900 dark:text-white text-3xl sm:text-5xl">
+                  Get in Touch
+                </h2>
+
+              </div>
+            </div>
+            <div className="flex items-stretch justify-center">
+              <div className="grid md:grid-cols-2">
+                <div className="h-full pr-6">
+                  <p className="mt-3 mb-12 text-lg text-gray-600 dark:text-slate-400">
+                    Feel free to reach out to us! Whether you have a question, feedback, or a collaboration proposal, we'd love to hear from you.
+                  </p>
+                  <ul className="mb-6 md:mb-0">
+                    <li className="flex">
+                      <div className="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width={24}
+                          height={24}
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth={2}
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          className="h-6 w-6"
+                        >
+                          <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+                          <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z"></path>
+                        </svg>
+                      </div>
+                      <div className="ml-4 mb-4">
+                        <h3 className="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">
+                          Our Address
+                        </h3>
+                        <p className="text-gray-600 dark:text-slate-400">
+                          2nd Floor 201 Homeland 7th Street
+                        </p>
+                        <p className="text-gray-600 dark:text-slate-400">
+                          Delhi ,100141
+                        </p>
+                      </div>
+                    </li>
+                    <li className="flex">
+                      <div className="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width={24}
+                          height={24}
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth={2}
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          className="h-6 w-6"
+                        >
+                          <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"></path>
+                          <path d="M15 7a2 2 0 0 1 2 2" />
+                          <path d="M15 3a6 6 0 0 1 6 6" />
+                        </svg>
+                      </div>
+                      <div className="ml-4 mb-4">
+                        <h3 className="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">
+                          Contact
+                        </h3>
+                        <p className="text-gray-600 dark:text-slate-400">
+                          Mobile: +91 888-4334-4545
+                        </p>
+                        <p className="text-gray-600 dark:text-slate-400">
+                          Mail: nukkadblog.com
+                        </p>
+                      </div>
+                    </li>
+                    <li className="flex">
+                      <div className="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width={24}
+                          height={24}
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth={2}
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          className="h-6 w-6"
+                        >
+                          <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+                          <path d="M12 7v5l3 3" />
+                        </svg>
+                      </div>
+                      <div className="ml-4 mb-4">
+                        <h3 className="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">
+                          Working hours
+                        </h3>
+                        <p className="text-gray-600 dark:text-slate-400">
+                          Monday - Friday: 09:00am - 08:00pm
+                        </p>
+                        <p className="text-gray-600 dark:text-slate-400">
+                          Saturday &amp; Sunday: 08:00am - 10:00pm
+                        </p>
+                        <p>
+                          <>
+                            <div className="ml-4 mb-4"></div>
+                            <p>
+                              <h3 className="mb-1 text-lg font-medium leading-6 text-gray-900 dark:text-white">
+                                Connect with us on social media </h3>
+                            </p>
+                            <span className="inline-flex mt-6 justify-center sm:justify-start">
+                              <a className="text-gray-500 hover:text-gray-900" target="_blank"
+                                href="https://twitter.com/example"
+                              >
+                                <svg
+                                  fill="currentColor"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth={2}
+                                  className="w-6 h-6"
+                                  viewBox="0 0 24 24"
+                                >
+                                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                                </svg>
+                              </a>
+                              <a
+                                className="ml-3 text-gray-500 hover:text-gray-900"
+                                href="https://www.instagram.com/example/"
+                                target="_blank"
+                              >
+                                <svg
+                                  fill="none"
+                                  stroke="currentColor"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth={2}
+                                  className="w-6 h-6"
+                                  viewBox="0 0 24 24"
+                                >
+                                  <rect width={20} height={20} x={2} y={2} rx={5} ry={5} />
+                                  <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
+                                </svg>
+                              </a>
+                            </span>
+                          </>
+
+                        </p>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+                <div className="card h-fit max-w-6xl p-5 md:p-12" id="form">
+                  <h2 className="mb-4 text-2xl font-bold dark:text-white">
+                    Contact us
+                  </h2>
+                  <form id="contactForm">
+                    <div className="mb-6">
+                      <div className="mx-0 mb-1 sm:mb-4">
+                        <div className="mx-0 mb-1 sm:mb-4">
+                          <label
+                            htmlFor="name"
+                            className="pb-1 text-xs uppercase tracking-wider"
+                          />
+                          <input
+                            type="text"
+                            id="name"
+                            autoComplete="given-name"
+                            placeholder="Your name"
+                            className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
+                            name="name"
+                          />
+                        </div>
+                        <div className="mx-0 mb-1 sm:mb-4">
+                          <label
+                            htmlFor="email"
+                            className="pb-1 text-xs uppercase tracking-wider"
+                          />
+                          <input
+                            type="email"
+                            id="email"
+                            autoComplete="email"
+                            placeholder="Your email address"
+                            className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
+                            name="email"
+                          />
+                        </div>
+                      </div>
+                      <div className="mx-0 mb-1 sm:mb-4">
+                        <label
+                          htmlFor="textarea"
+                          className="pb-1 text-xs uppercase tracking-wider"
+                        />
+                        <textarea
+                          id="textarea"
+                          name="textarea"
+                          cols={30}
+                          rows={5}
+                          placeholder="Write your message..."
+                          className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
+                          defaultValue={""}
+                        />
+                      </div>
+                    </div>
+                    <div className="text-center">
+                      <button
+                        type="submit"
+                        className="w-full bg-blue-800 text-white px-6 py-3 font-xl rounded-md sm:mb-0"
+                      >
+                        Send
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+      </>
+    </div>
   )
 }
 
-export default Contact
+export default Contact;
