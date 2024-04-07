9477c82e40e42276a368cb320d6ca933fc3eab88
sahilmayank <sahilmayank3@gmail.com>
2024-04-07 12:18:51 +0530

    ..

---
+++ backend/Models/competitionModel.js
@@ -0,0 +1,9 @@
+const { Schema, model } = require('../connection');
+
+const mySchema = new Schema({
+    username : {type : String, required : true},
+    email:String,
+    password:String,
+});
+
+module.exports = model('competition', mySchema);
--- backend/Models/userModel.js
+++ backend/Models/userModel.js
@@ -1,9 +1,12 @@
 const { Schema, model } = require('../connection');
 
 const mySchema = new Schema({
-    username : {type : String, required : true},
-    email:String,
-    password:String,
+    name: { type: String, required: true },
+    email: String,
+    password: String,
+    avatar: String,
+    role: { type: String, default: 'user' },
+    created: { type: Date, default: Date.now }
 });
 
 module.exports = model('user', mySchema);
