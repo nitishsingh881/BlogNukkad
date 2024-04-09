8b5b0c048915609091761e778f9a300fd7def148
nitish singh <itsnitish881@gmail.com>
2024-04-09 11:29:19 +0530

    added blog model

--- frontend/src/app/(main)/layout.jsx
+++ frontend/src/app/(main)/layout.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Footer from '../components/footer';
 import Navbar from './navbar';
 
 const MainLayout = ({ children }) => {
@@ -6,6 +7,7 @@ const MainLayout = ({ children }) => {
         <>
             <Navbar />
             {children}
+            <Footer/>
         </>
     )
 }
--- frontend/src/app/components/navbar.jsx
+++ frontend/src/app/components/navbar.jsx
@@ -1,65 +1,91 @@
 import React from 'react'
 
-const AdminNavbar = () => {
+const Navbar = () => {
   return (
     <div>
       <>
-      <header className="sticky inset-0 z-50 border-b border-slate-100 bg-white/80 backdrop-blur-lg">
-  <nav className="mx-auto flex max-w-6xl gap-8 px-6 transition-all duration-200 ease-in-out lg:px-12 py-4">
-    <div className="relative flex items-center">
-      <a href="/">
-        <img
-          src="https://www.svgrepo.com/show/499831/target.svg"
-          loading="lazy"
-          style={{ color: "transparent" }}
-          width={32}
-          height={32}
-        />
-      </a>
-    </div>
-    <ul className="hidden items-center justify-center gap-6 md:flex">
-      <li className="pt-1.5 font-dm text-sm font-medium text-slate-700">
-        <a href="#">Pricing</a>
-      </li>
-      <li className="pt-1.5 font-dm text-sm font-medium text-slate-700">
-        <a href="#">Blog</a>
-      </li>
-      <li className="pt-1.5 font-dm text-sm font-medium text-slate-700">
-        <a href="#">Docs</a>
-      </li>
-    </ul>
-    <div className="flex-grow" />
-    <div className="hidden items-center justify-center gap-6 md:flex">
-      <a href="#" className="font-dm text-sm font-medium text-slate-700">
-        Sign in
-      </a>
-      <a
-        href="#"
-        className="rounded-md bg-gradient-to-br from-green-600 to-emerald-400 px-3 py-1.5 font-dm text-sm font-medium text-white shadow-md shadow-green-400/50 transition-transform duration-200 ease-in-out hover:scale-[1.03]"
-      >
-        Sign up for free
-      </a>
-    </div>
-    <div className="relative flex items-center justify-center md:hidden">
-      <button type="button">
+      <header className="shadow-md py-4 px-4 sm:px-10 bg-white font-sans min-h-[70px]">
+  <div className="flex flex-wrap items-center gap-4">
+    <a href="javascript:void(0)">
+      <img
+        src="https://readymadeui.com/readymadeui.svg"
+        alt="logo"
+        className="w-36"
+      />
+    </a>
+    <div className="flex ml-auto lg:order-1">
+      <button className="px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#007bff] bg-[#007bff] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#007bff]">
+        Login
+      </button>
+      <button className="px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#007bff] bg-[#007bff] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#007bff] ml-4">
+        Sign up
+      </button>
+      <button className="lg:hidden ml-7">
         <svg
+          className="w-7 h-7"
+          fill="#000"
+          viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          aria-hidden="true"
-          className="h-6 w-auto text-slate-900"
         >
           <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+            clipRule="evenodd"
           />
         </svg>
       </button>
     </div>
-  </nav>
+    <ul className="flex lg:ml-12 lg:space-x-4 max-lg:space-y-2 max-lg:block max-lg:w-full">
+      <li className="max-lg:border-b max-lg:bg-[#007bff] max-lg:py-2 px-3 max-lg:rounded">
+        <a
+          href="javascript:void(0)"
+          className="lg:hover:text-[#007bff] text-[#007bff] max-lg:text-white block font-semibold text-[15px]"
+        >
+          Home
+        </a>
+      </li>
+      <li className="max-lg:border-b max-lg:py-2 px-3 max-lg:rounded">
+        <a
+          href="javascript:void(0)"
+          className="lg:hover:text-[#007bff] text-black block font-semibold text-[15px]"
+        >
+          Team
+        </a>
+      </li>
+      <li className="max-lg:border-b max-lg:py-2 px-3 max-lg:rounded">
+        <a
+          href="javascript:void(0)"
+          className="lg:hover:text-[#007bff] text-black block font-semibold text-[15px]"
+        >
+          Feature
+        </a>
+      </li>
+      <li className="max-lg:border-b max-lg:py-2 px-3 max-lg:rounded">
+        <a
+          href="javascript:void(0)"
+          className="lg:hover:text-[#007bff] text-black block font-semibold text-[15px]"
+        >
+          Blog
+        </a>
+      </li>
+      <li className="max-lg:border-b max-lg:py-2 px-3 max-lg:rounded">
+        <a
+          href="javascript:void(0)"
+          className="lg:hover:text-[#007bff] text-black block font-semibold text-[15px]"
+        >
+          About
+        </a>
+      </li>
+      <li className="max-lg:border-b max-lg:py-2 px-3 max-lg:rounded">
+        <a
+          href="javascript:void(0)"
+          className="lg:hover:text-[#007bff] text-black block font-semibold text-[15px]"
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  </div>
 </header>
 
       </>
@@ -67,4 +93,4 @@ const AdminNavbar = () => {
   )
 }
 
-export default AdminNavbar;
+export default Navbar;
