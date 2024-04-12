1a7bba5519b2087cc50026f59b3c44e1816fd3ec
nitish singh <itsnitish881@gmail.com>
2024-04-12 11:37:41 +0530

    added competition details page

--- backend/Routers/userRouter.js
+++ backend/Routers/userRouter.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const Model = require('../Models/userModel');
 
+const jwt = require('jsonwebtoken');
+require
+
 router.post('/add', (req, res) => {
     console.log(req.body);
     new Model(req.body).save()
@@ -38,11 +41,41 @@ router.delete('/delete/:id', (req, res) => {
 router.put('/update/:id', (req, res) => {
     Model.findByIdAndUpdate(req.params.id, req.body)
     .then((result) => {
+        
         res.status(200).json(result);
     }).catch((err) => {
         console.log(err);
         res.status(500).json(err);
+
     });
 });
+
+router.post('/authenticate', (req,res) => {
+    console.log(req.body);
+    then((result) =>{
+        if(result){
+        const { _id, name , email} =result;
+        const payload ={_id, name, email};
+        jwt.sign(
+            payload.env.JWT_SECRET,
+            {expiry: '2 days'},
+            (err,token)=> {
+        
+            if(err){
+                res.status(500).json({message:'error creating token'})
+            }else{
+                res.status(200).json({token, role: result.role})
+            }
+
+            }
+
+        )
+    }else{
+        res.status(401).json({message:'Invalid Cre'})
+    }
+}).catch((err) => {
+    req.status(500).json(err)
+})
+})
 
 module.exports = router;
